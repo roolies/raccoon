b80ecc41d326d107a177723b05cc3e529b9f6c2d
CasasJc <jesuscasas@knights.ucf.edu>
2022-11-18 13:22:56 -0500

    JC Shoot object update

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -11,18 +11,23 @@ public class Weapon : MonoBehaviour
 	public bool isShooting;
 
 	public Transform shootPos;
-	public GameObject bullet;
+	public GameObject Can,Bannana,Apple;
 	public Ammo ammo;
+
+	public float CanNumber;
 
 	void Start()
 	{
-
+		CanNumber = 1;
 	}
 
 	void Update()
 	{
 
-
+		if(CanNumber > 3)
+        {
+			CanNumber = 1;
+        }
 		
 
 
@@ -32,11 +37,13 @@ public class Weapon : MonoBehaviour
 	{
 		if (context.performed)
 		{
-			if(ammo.CurrentAmmo >= 0)
+			if(ammo.CurrentAmmo > 0)
             {
 				StartCoroutine(Shoot());
+				CanNumber += 1;
+			}
 
-            }
+			
 
 		}
 
@@ -60,11 +67,33 @@ public class Weapon : MonoBehaviour
             }
         }
 
-		isShooting = true;
-		GameObject newBullet = Instantiate(bullet, shootPos.position, Quaternion.identity);
-		newBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * direction() * Time.fixedDeltaTime, 0f);
-		newBullet.transform.localScale = new Vector2(newBullet.transform.localScale.x * direction(), newBullet.transform.localScale.y);
+		
 
+		if(CanNumber == 1)
+        {
+			GameObject NewCan = Instantiate(Can, shootPos.position, Quaternion.identity);
+			NewCan.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * direction() * Time.fixedDeltaTime, 0f);
+			NewCan.transform.localScale = new Vector2(NewCan.transform.localScale.x * direction(), NewCan.transform.localScale.y);
+        }
+
+        if (CanNumber == 2)
+        {
+			GameObject NewBannana = Instantiate(Bannana, shootPos.position, Quaternion.identity);
+			NewBannana.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * direction() * Time.fixedDeltaTime, 0f);
+			NewBannana.transform.localScale = new Vector2(NewBannana.transform.localScale.x * direction(), NewBannana.transform.localScale.y);
+        }
+
+		if(CanNumber == 3)
+        {
+			GameObject NewApple = Instantiate(Apple, shootPos.position, Quaternion.identity);
+			NewApple.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * direction() * Time.fixedDeltaTime, 0f);
+			NewApple.transform.localScale = new Vector2(NewApple.transform.localScale.x * direction(), NewApple.transform.localScale.y);
+        }
+
+
+
+
+		ammo.CurrentAmmo -= 1;
 		Debug.Log("Shoot");
 		yield return new WaitForSeconds(shootTimer);
 		isShooting = false;
